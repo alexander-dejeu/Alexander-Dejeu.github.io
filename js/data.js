654da9f27b748058a9c443f1ff050e2faf194db6
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-24 11:58:16 -0800

    fixed up handel bars and updated how I am saving structured data

--- js/data.js
+++ js/data.js
@@ -1,26 +1,38 @@
 var data = [
     {id: 1,
      title: "My Visual Partner",
+     short_title: "MVP",
+     type: "iOS_app",
+     icon: "Icon.jpg",
      tagline: "It is like uber for tinder",
      date: "September 2016",
      description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
-     logo: "appleLogo"
+     logo: "appleLogo",
+     detailed_description: "MVP - My Visual Partner provides a system that encourages independence for getting ready for user defined events. <br /><br />MVP allows users to create and schedule events. Then the user specifies the items that are required for the event. Once the event is created the user will be promoted and have a visual checklist to insure they find all of his or her items at the events specified time."
     }, 
     {
      id: 2,
      title: "ICE",
+     short_title: "ICE",
+     type: "iOS_app",
+     icon: "Icon.jpg",
      tagline: "It is like uber for tinder",
      date: "September 2016",
      description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
-     logo: "appleLogo"
+     logo: "appleLogo",
+     detailed_description: "Here is the super detailed description..."
     }, 
     {
      id: 3,
      title: "IT worked!!",
+     short_title: "DoctorWatson",
+     icon: "Icon.jpg",
+     type: "iOS_app",
      tagline: "It is like uber for tinder",
      date: "September 2016",
      description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
-     logo: "appleLogo"
+     logo: "appleLogo",
+     detailed_description: "Here is the super detailed description..."
     }
 ];
 
