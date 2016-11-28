7c68604bc2b7a8901433b612ee269c2358d929b7
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-27 21:07:49 -0800

    cleaned up comments, beautified the popup, and added the actionables to the popup

--- js/data.js
+++ js/data.js
@@ -1,37 +1,37 @@
 var data = [
     {id: 1,
      title: "My Visual Partner",
+     featured: true,
      short_title: "MVP",
      type: "iOS_app",
      icon: "Icon.jpg",
      tagline: "It is like uber for tinder",
      date: "September 2016",
      description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
-     logo: "appleLogo",
      detailed_description: "MVP - My Visual Partner provides a system that encourages independence for getting ready for user defined events. <br /><br />MVP allows users to create and schedule events. Then the user specifies the items that are required for the event. Once the event is created the user will be promoted and have a visual checklist to insure they find all of his or her items at the events specified time."
     }, 
     {
      id: 2,
      title: "ICE",
+     featured: false,
      short_title: "ICE",
      type: "iOS_app",
      icon: "Icon.jpg",
      tagline: "It is like uber for tinder",
      date: "September 2016",
      description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
-     logo: "appleLogo",
      detailed_description: "Here is the super detailed description..."
     }, 
     {
      id: 3,
      title: "IT worked!!",
+     featured: false,
      short_title: "DoctorWatson",
      icon: "Icon.jpg",
      type: "iOS_app",
      tagline: "It is like uber for tinder",
      date: "September 2016",
      description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
-     logo: "appleLogo",
      detailed_description: "Here is the super detailed description..."
     }
 ];
