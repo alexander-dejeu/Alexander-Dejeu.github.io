d2cc0bb91a3cee4bcb88eca1a22c17c7facbc76c
Alexander Dejeu <alexdejeu@gmail.com>
2016-11-30 01:07:51 -0800

    added Resume PDF

--- js/data.js
+++ js/data.js
@@ -9,8 +9,9 @@ var data = [
      date: "September 2016",
      description: "iOS app that provides a system to encourage independence for individuals with learning differences",
      detailed_description: "MVP - My Visual Partner provides a system that encourages independence for getting ready for user defined events. <br /><br />MVP allows users to create and schedule events. Then the user specifies the items that are required for the event. Once the event is created the user will be promoted and have a visual checklist to insure they find all of his or her items at the events specified time.",
-     website_link: "google.com",
-     live_product_link: "Google"
+     website_link: "http://myvisualpartner.com",
+     websiteIsArticle: false,
+     live_product_link: "https://itunes.apple.com/us/app/mvp-my-visual-partner/id1052876510?mt=8"
     }, 
     {
      id: 2,
@@ -23,35 +24,38 @@ var data = [
      date: "August 2015",
      description: "ICE - Interactive Collaborative Environment provides a platform that delivers Reminders, Scheduling, an Aggregated Dashboard, and Collaboration!",
      detailed_description: "Here is the super detailed description...",
-     website_link: "google.com",
-     live_product_link: "Google"
+     website_link: "http://dogoodtechnology.com",
+     websiteIsArticle: false,
+     live_product_link: "https://itunes.apple.com/sb/app/ice-interactive-collaborative/id1030680444?mt=8"
     }, 
     {
      id: 3,
-     title: "Best Mobile App Cal Hacks 3.0",
+     title: "Best Mobile App",
      featured: false,
      short_title: "TravelChest",
      icon: "Icon.jpg",
      type: "award",
      tagline: "It is like uber for tinder",
      date: "November 2016",
-     description: "You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
+     description: "Cal Hacks 3.0 You can now write a very nice beautiful discritpion that includes many important details and whatnot about the project.",
      detailed_description: "Here is the super detailed description...",
      website_link: "https://devpost.com/software/travel-chest-pdye6a",
+     websiteIsArticle: false,
      live_product_link: ""
     }, 
     {
      id: 4,
-     title: "Most Polished App at HackBCA III",
+     title: "Most Polished App",
      featured: false,
      short_title: "DoctorWatson",
      icon: "Icon.jpg",
      type: "award",
      tagline: "Doctor Watson - Receive professional medical feedback instantly",
      date: "April 2016",
-     description: "DoctorWatson is an iOS app that utilizes the power of IMB's Watson AI to process the users naturally spoken medical complaint and then match the user with the most relevant doctor available",
+     description: "Awarded at HackBCA III for DoctorWatson, an iOS app that utilizes AI to process the users naturally spoken medical complaint and then matchs the user with a relevant and avaliable doctor.",
      detailed_description: "DoctorWatson utilizes the power of IMB's Watson AI to process the users naturally spoken medical complaint. Then through mapping the words and phrases through relevance we determined the best fit specialist type to assist the user. Next we parse through our database of Doctor users and find a doctor that is free and can best assist the user. We then show the best fit doctor to the user, if the user likes the doctor they can then chat and discuss the health issue.",
      website_link: "https://devpost.com/software/doctorwatson-9rpvot",
+     websiteIsArticle: false,
      live_product_link: ""
     }, 
     {
@@ -62,10 +66,11 @@ var data = [
      icon: "Icon.jpg",
      type: "iOS_app",
      tagline: "Prepare for college apps with High School LaVie",
-     date: "September 2016",
+     date: "February 2016",
      description: "High School LaVie is an iOS app that keeps track of essential elements and streamlines the college application process.",
      detailed_description: "High School LaVie allows students to effectively and elegantly record high school achievements. This is critically important for the college application process and prevents an \'Oh shoot\' moment when you are applying for college and you forget what spectacular things you were doing back in 9th grade. HS LaVie solves this problem :) Quickly, create, save, and view activities. Reminders, will encourage you to do little bits every so often in order to prevent forgetting or putting off recording your achievements. <br /><br /> Download High School LaVie and become more prepared for the college application process.",
      website_link: "http://mylavie.com",
+     websiteIsArticle: false,
      live_product_link: "https://itunes.apple.com/us/app/high-school-lavie/id1086503521?mt=8"
     },
     {
@@ -76,11 +81,12 @@ var data = [
      icon: "Icon.jpg",
      type: "iOS_app",
      tagline: "Prepare for annual reviews with Professional LaVie",
-     date: "September 2016",
+     date: "July 2016",
      description: "High School LaVie is an iOS app that keeps track of essential elements and streamlines the college application process.",
      detailed_description: "High School LaVie allows students to effectively and elegantly record high school achievements. This is critically important for the college application process and prevents an \'Oh shoot\' moment when you are applying for college and you forget what spectacular things you were doing back in 9th grade. HS LaVie solves this problem :) Quickly, create, save, and view activities. Reminders, will encourage you to do little bits every so often in order to prevent forgetting or putting off recording your achievements. <br /><br /> Download High School LaVie and become more prepared for the college application process.",
-     website_link: "google.com",
-     live_product_link: "Google"
+     website_link: "",
+     websiteIsArticle: false,
+     live_product_link: "https://itunes.apple.com/us/app/professional-lavie-organize/id1131868387?mt=8"
     },
     {
      id: 7,
@@ -94,12 +100,13 @@ var data = [
      description: "High School LaVie is an iOS app that keeps track of essential elements and streamlines the college application process.",
      detailed_description: "Provides connections to skilled individuals in the area for those who require the completion of a specific task. The functionality of the application is divided into the medical requests, the miscellaneous utility requests, and the social requests. The user places a Beacon for a specific problem they desire completed. All individuals with the application downloaded within a preset radius and fulfilling a basic skill requirement are notified of the Beacon. When installing the application, the user inputs their skills. Sensitive skills such as emergency medical abilities are vetted.",
      website_link: "https://devpost.com/software/beacons",
+     websiteIsArticle: false,
      live_product_link: ""
     },
     {
      id: 8,
      title: "How to build a great product series",
-     featured: false,
+     featured: true,
      short_title: "ProLaVie",
      icon: "Icon.jpg",
      type: "medium_article",
@@ -107,12 +114,28 @@ var data = [
      date: "November 2016",
      description: "High School LaVie is an iOS app that keeps track of essential elements and streamlines the college application process.",
      detailed_description: "High School LaVie allows students to effectively and elegantly record high school achievements. This is critically important for the college application process and prevents an \'Oh shoot\' moment when you are applying for college and you forget what spectacular things you were doing back in 9th grade. HS LaVie solves this problem :) Quickly, create, save, and view activities. Reminders, will encourage you to do little bits every so often in order to prevent forgetting or putting off recording your achievements. <br /><br /> Download High School LaVie and become more prepared for the college application process.",
-     website_link: "google.com",
-     live_product_link: "Google"
+     website_link: "https://medium.com/@alexdejeu/cal-hacks-3-0-calling-all-hackers-dreamers-4-a-mers-c96c29b510e9#.qw4hvrkin",
+     websiteIsArticle: true,
+     live_product_link: ""
     },
     {
      id: 9,
-     title: "How to build a great product series",
+     title: "Google Glass Glasswear",
+     featured: true,
+     short_title: "ProLaVie",
+     icon: "Icon.jpg",
+     type: "medium_article",
+     tagline: "Prepare for annual reviews with Professional LaVie",
+     date: "November 2016",
+     description: "High School LaVie is an iOS app that keeps track of essential elements and streamlines the college application process.",
+     detailed_description: "High School LaVie allows students to effectively and elegantly record high school achievements. This is critically important for the college application process and prevents an \'Oh shoot\' moment when you are applying for college and you forget what spectacular things you were doing back in 9th grade. HS LaVie solves this problem :) Quickly, create, save, and view activities. Reminders, will encourage you to do little bits every so often in order to prevent forgetting or putting off recording your achievements. <br /><br /> Download High School LaVie and become more prepared for the college application process.",
+     website_link: "https://medium.com/@alexdejeu/cal-hacks-3-0-calling-all-hackers-dreamers-4-a-mers-c96c29b510e9#.qw4hvrkin",
+     websiteIsArticle: true,
+     live_product_link: ""
+    },
+    {
+     id: 10,
+     title: "Cal Hacks 3.0",
      featured: false,
      short_title: "ProLaVie",
      icon: "Icon.jpg",
@@ -121,7 +144,8 @@ var data = [
      date: "November 2016",
      description: "High School LaVie is an iOS app that keeps track of essential elements and streamlines the college application process.",
      detailed_description: "High School LaVie allows students to effectively and elegantly record high school achievements. This is critically important for the college application process and prevents an \'Oh shoot\' moment when you are applying for college and you forget what spectacular things you were doing back in 9th grade. HS LaVie solves this problem :) Quickly, create, save, and view activities. Reminders, will encourage you to do little bits every so often in order to prevent forgetting or putting off recording your achievements. <br /><br /> Download High School LaVie and become more prepared for the college application process.",
-     website_link: "google.com",
-     live_product_link: "Google"
+     website_link: "https://medium.com/@alexdejeu/product-development-the-tools-to-build-a-successful-product-e906ac362ca6#.rne622k3k",
+     websiteIsArticle: true,
+     live_product_link: ""
     }
 ];
